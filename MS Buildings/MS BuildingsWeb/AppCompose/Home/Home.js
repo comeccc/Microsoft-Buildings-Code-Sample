9e85f3abbc86802c840685733c12e120d07eacdd
Jeremy Thake <jthake@made4the.net>
2015-10-12 00:54:20 -0700

    Cleaned up the sample code from default project that is redundant in this sample.

--- MS Buildings/MS BuildingsWeb/AppCompose/Home/Home.js
+++ MS Buildings/MS BuildingsWeb/AppCompose/Home/Home.js
@@ -8,25 +8,8 @@
         $(document).ready(function () {
             app.initialize();
             loadBuildings();
-            $('#set-subject').click(setSubject);
-            $('#get-subject').click(getSubject);
-            $('#add-to-recipients').click(addToRecipients);
         });
     };
-
-    function setSubject() {
-        Office.cast.item.toItemCompose(Office.context.mailbox.item).subject.setAsync("Hello world!");
-    }
-
-    function getSubject() {
-        Office.cast.item.toItemCompose(Office.context.mailbox.item).subject.getAsync(function (result) {
-            app.showNotification('The current subject is', result.value)
-        });
-    }
-
-    function addToRecipients() {
-        Office.cast.item.toItemCompose(Office.context.mailbox.item).to.addAsync([Office.context.mailbox.userProfile.emailAddress]);
-    }
 
 })();
 
